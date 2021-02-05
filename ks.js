d9e705ca8428ad4b2f08619af02a8140fbdad420
MAK <9234mk02@gmail.com>
2021-02-06 00:35:14 +0530

    ks fix

--- ks.js
+++ ks.js
@@ -8,14 +8,14 @@ module.exports = function (message) {
     }
     else if (kisko == message.author.username) {
         message.channel.send("oof ")
-    }
-    else if(kisko == client.user.username) message.reply("no")
+    }    
+    else if(message.mentions.users.first().id == 806450964156252201) message.channel.send("no")
     else {
         let ran = Math.floor(Math.random() * 17)+1; 
         const h = new Discord.MessageEmbed()
             .setTitle(`${message.author.username} kisses ${kisko}`)
             .setColor('#FF0000')
             .setImage(`https://acegif.com/wp-content/uploads/anime-kissin-${ran}.gif`)
-        message.reply(h)
+        message.channel.send(h)
     }
 }
