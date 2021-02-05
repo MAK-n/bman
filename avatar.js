23231c403d81b6c976a28377189bfce03b816c0a
MAK <9234mk02@gmail.com>
2021-02-06 01:05:25 +0530

    av fix

--- avatar.js
+++ avatar.js
@@ -1,27 +1,20 @@
 module.exports = function (message) {
     let ping = message.mentions.users.first();
-    if (ping !== undefined) {
-        const avatar = new Discord.MessageEmbed()
-            .setTitle(ping.username + "'s avatar")
-            .setColor("#FF0000")
-            .setImage(ping.displayAvatarURL({
-                dynamic: true,
-                format: "png",
-                size: 512
-            }))
-
-        message.reply(avatar)
-    }
-    else {
+    if (ping == undefined) {
         const avatar = new Discord.MessageEmbed()
             .setTitle(message.author.username + "'s avatar")
             .setColor("#FF0000")
-            .setImage(message.author.displayAvatarURL({
-                dynamic: true,
-                format: "png",
-                size: 512
-            }))
+            .setImage(message.author.displayAvatarURL({size: 1024}))
 
-        message.reply(avatar)
+        message.channel.send(avatar)       
+        
+    }
+    else {
+        const avatar = new Discord.MessageEmbed()
+            .setTitle(ping.username + "'s avatar")
+            .setColor("#FF0000")
+            .setImage(ping.displayAvatarURL({size: 1024}))
+
+        message.channel.send(avatar)
     }
 }
