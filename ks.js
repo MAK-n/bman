5fa547fe52fe8a5ab3a73d3e8d6e531bb57f4783
MAK <9234mk02@gmail.com>
2021-02-06 17:46:01 +0530

    no

--- ks.js
+++ ks.js
@@ -9,6 +9,7 @@ module.exports = function (message) {
     else if (kisko == message.author.username) {
         message.channel.send("naah :/")
     }
+    else if (message.mentions.users.first().id == "806450964156252201") message.channel.send("no")
     else {
         let ran = Math.floor(Math.random() * 17)+1;        
         const h = new Discord.MessageEmbed()
