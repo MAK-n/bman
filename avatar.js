01e8f1a0b65fc8ed343e077fd50fc957b7749a31
MAK <9234mk02@gmail.com>
2021-02-11 00:17:52 +0530

    av fiz

--- avatar.js
+++ avatar.js
@@ -1,19 +1,23 @@
 module.exports = function (message) {
     let ping = message.mentions.users.first();
     if (ping == undefined) {
+        let img= message.author.displayAvatarURL()
+        img= img.subString(0,(img.length()-4))+"1024"
         const avatar = new Discord.MessageEmbed()
             .setTitle(message.author.username + "'s avatar")
             .setColor("#FF0000")
-            .setImage(message.author.displayAvatarURL({size: 1024}))
+            .setImage(img)
 
         message.channel.send(avatar)       
         
     }
     else {
+        let img= ping.displayAvatarURL()
+        img= img.subString(0,(img.length()-4))+"1024"
         const avatar = new Discord.MessageEmbed()
             .setTitle(ping.username + "'s avatar")
             .setColor("#FF0000")
-            .setImage(ping.displayAvatarURL({size: 1024}))
+            .setImage(img)
 
         message.channel.send(avatar)
     }
