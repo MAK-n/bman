48ad0476e34d792f0c8e43d2b2cf969aeae8cfc7
MAK <9234mk02@gmail.com>
2021-02-11 00:38:45 +0530

    av fix

--- avatar.js
+++ avatar.js
@@ -1,24 +1,25 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+
 module.exports = function (message) {
     let ping = message.mentions.users.first();
     if (ping == undefined) {
         let img= message.author.displayAvatarURL()
-        img= img.subString(0,(img.length()-4))+"1024"
+        img= img+"?size=1024"
         const avatar = new Discord.MessageEmbed()
             .setTitle(message.author.username + "'s avatar")
             .setColor("#FF0000")
             .setImage(img)
-
         message.channel.send(avatar)       
         
     }
     else {
         let img= ping.displayAvatarURL()
-        img= img.subString(0,(img.length()-4))+"1024"
+        img= img+"?size=1024"
         const avatar = new Discord.MessageEmbed()
             .setTitle(ping.username + "'s avatar")
             .setColor("#FF0000")
             .setImage(img)
-
         message.channel.send(avatar)
     }
 }
