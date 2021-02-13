985ab4584e5ae11945ff128d6e5372cf7516bd5f
MAK <9234mk02@gmail.com>
2021-02-14 00:24:12 +0530

    undefined kick or ban fix

--- ban.js
+++ ban.js
@@ -1,9 +1,9 @@
 module.exports = function (message) {
     if (message.mentions.members.first() != undefined) {
         if (message.member.hasPermission('BAN_MEMBERS')) {
-            if (message.mentions.members.first().hasPermission('ADMINISTRATOR')) { message.channel.send("admin hai wo kar ban skta hai to karle ¯\_(ツ)_/¯") }
+            if (message.mentions.members.first().hasPermission('ADMINISTRATOR')) { message.channel.send("admin hai wo ¯\_(ツ)_/¯") }
             else {
-                let banh = message.mentions.members.first().username
+                let banh = message.mentions.users.first().username
                 message.mentions.members.first().ban();
                 message.channel.send(banh + ' was banned')
             }
--- kick.js
+++ kick.js
@@ -3,7 +3,7 @@ module.exports = function (message) {
         if (message.member.hasPermission('KICK_MEMBERS')) {
             if (message.mentions.members.first().hasPermission('ADMINISTRATOR')) { message.channel.send("admin hai to kar skta kick hai to karle ¯\_(ツ)_/¯") }
             else {
-                let kickh = message.mentions.members.first().username
+                let kickh = message.mentions.users.first().username
                 message.mentions.members.first().kick();
                 message.channel.send(kickh + ' was kicked')
             }
